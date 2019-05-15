1018ec24f0bded2f1c4ece1f4983a99b8a2f9ad8
nemanjasipka <nemanja.sipka31@gmail.com>
2019-05-15 17:20:50 +0200

    popup style for desktop
    creating pop-up finction and css for it

---
+++ src/js/popup.js
@@ -0,0 +1,9 @@
+// When the user clicks on div, open the popup
+
+    function myFunction() {
+        var popup = document.getElementById("myPopup");
+        var cover = document.getElementById("cover");
+        popup.classList.toggle("show");
+        cover.classList.toggle("cover-visible");
+    }
+
